f85bb18320e7a9c9bead72936be175ba6ef22ae0
thiagohenrique7 <thiagohenriquean@gmail.com>
2023-08-01 23:07:20 -0300

    commit 1

---
+++ TestDollar.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestDollar {
+  @Test
+  public void testMultiplication() {
+    Dollar five = new Dollar(5);
+    five.times(2);
+    assertEquals(10, five.amount);
+  }
+}

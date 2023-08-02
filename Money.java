ea8ac8c18708727705ab5d449cbf4495c6017a9d
thiagohenrique7 <thiagohenriquean@gmail.com>
2023-08-02 00:11:48 -0300

    commit 5

--- Money.java
+++ Money.java
@@ -1,4 +1,4 @@
-abstract class Money {
+class Money {
   protected int amount;
 
   protected String currency;
@@ -8,11 +8,11 @@ abstract class Money {
   }
 
   static Money dollar(int amount) {
-    return new Dollar(amount, "USD");
+    return new Money(amount, "USD");
   }
 
   static Money franc(int amount) {
-    return new Franc(amount, "CHF");
+    return new Money(amount, "CHF");
   }
 
   Money(int amount, String currency) {
@@ -20,10 +20,12 @@ abstract class Money {
     this.currency = currency;
   }
 
-  abstract Money times(int multiplier);
+  Money times(int multiplier) {
+    return new Money(amount * multiplier, currency);
+  }
 
   public boolean equals(Object object) {
     Money money = (Money) object;
-    return amount == money.amount && getClass().equals(money.getClass());
+    return amount == money.amount && currency().equals(money.currency());
   }
 }
--- TestClass.java
+++ TestClass.java
@@ -8,8 +8,11 @@ public class TestClass {
   public void testEquality() {
     assertTrue(Money.dollar(5).equals(Money.dollar(5)));
     assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-    assertTrue(Money.franc(5).equals(Money.franc(5)));
-    assertFalse(Money.franc(5).equals(Money.franc(6)));
     assertFalse(Money.franc(5).equals(Money.dollar(5)));
+  }
+
+  @Test
+  public void testDifferentClassEquality() {
+    assertTrue(new Money(10, "CHF").equals(new Money(10, "CHF")));
   }
 }
